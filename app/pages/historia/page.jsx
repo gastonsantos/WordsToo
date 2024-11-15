cd0dc7f5ee70b5161e622e9f7270e59ec4016b14
gastonsantos <gastons525@gmail.com>
2024-11-15 14:25:39 -0300

    s

--- app/pages/historia/page.jsx
+++ app/pages/historia/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef } from 'react';
 import DogFiccion from "@/components/perro/dog-ficcion";
 import DogLove from "@/components/perro/dog-love";
 import DogTerror from "@/components/perro/dog-terror";
