693b92974f34c3d3e1dc11b13d38e3800377fb3d
gastonsantos <gastons525@gmail.com>
2024-11-19 11:14:10 -0300

    cambio endpoint

--- services/historia/api.js
+++ services/historia/api.js
@@ -1,14 +1,14 @@
-import { API_BACK } from "@/config/constants";
+import { API, API_BACK } from "@/config/constants";
 import axios from 'axios';
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 
 async function sendHistoryToIa(data){
-  // Esto debe mostrar "https://wordstoo-back.onrender.com"
+ 
 
     try {
-     // const response = await axios.post("https://wordstoo-back.onrender.com/api/cohere/continuarHistoria", data);
-      const response = await axios.post(`${API_BACK}/api/cohere/continuarHistoria`, data);
+      const response = await axios.post("https://wordstoo-back.onrender.com/api/cohere/continuarHistoria", data);
+     // const response = await axios.post(`${API_BACK}/api/cohere/continuarHistoria`, data);
       if (response.data != null) {
         console.log(response);
         localStorage.setItem("contenido",response.data)
@@ -23,8 +23,8 @@ async function sendHistoryToIa(data){
 
   async function mejoraHistoria(data){
     try {
-      //const response = await axios.post("https://wordstoo-back.onrender.com/api/cohere/mejorarHistoria", data);
-      const response = await axios.post(API_BACK+'/api/cohere/mejorarHistoria', data);
+      const response = await axios.post("https://wordstoo-back.onrender.com/api/cohere/mejorarHistoria", data);
+      //const response = await axios.post(API_BACK+'/api/cohere/mejorarHistoria', data);
       
       if (response.data != null) {
         console.log(response);
