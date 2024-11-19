b688a786e31e7b849f359527b1a45868dbafa583
gastonsantos <gastons525@gmail.com>
2024-11-19 11:19:36 -0300

    s

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,4 @@
 export const API = process.env.NEXT_PUBLIC_API_BACKEND_DEVELOPMENT
 
-export const API_BACK = process.env.NEXT_PUBLIC_API_BACKEND
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_BACK = process.env.API_BACK_URL;
