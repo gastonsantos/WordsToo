0bb32ff31b09e8c2581a0c3cc9de9e1aceacbac8
gastonsantos <gastons525@gmail.com>
2024-11-23 20:46:10 -0300

    jwt

--- services/historia/api.js
+++ services/historia/api.js
@@ -8,7 +8,7 @@ async function sendHistoryToIa(data){
 
     try {
      const response = await axiosInstance.post("https://wordstoo-back.onrender.com/api/cohere/continuarHistoria", data);
-     //const response = await axiosInstance.post('http://localhost:8000/api/cohere/continuarHistoria', data);
+    // const response = await axiosInstance.post('http://localhost:8000/api/cohere/continuarHistoria', data);
       if (response.data != null) {
         console.log(response);
         localStorage.setItem("contenido",response.data)
@@ -23,8 +23,8 @@ async function sendHistoryToIa(data){
 
   async function mejoraHistoria(data){
     try {
-      //const response = await axiosInstance.post("https://wordstoo-back.onrender.com/api/cohere/mejorarHistoria", data);
-      const response = await axiosInstance.post('http://localhost:8000/api/cohere/mejorarHistoria', data);
+      const response = await axiosInstance.post("https://wordstoo-back.onrender.com/api/cohere/mejorarHistoria", data);
+      //const response = await axiosInstance.post('http://localhost:8000/api/cohere/mejorarHistoria', data);
       
       if (response.data != null) {
         console.log(response);
